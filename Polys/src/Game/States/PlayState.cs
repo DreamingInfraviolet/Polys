0f644936e2791468b649a70911931336d747122b
Anima <lightofanima@gmail.com>
2015-11-02 15:17:27 +0000

    Implemented script-based font initialisation
    In a bitmap font, characters must be mapped to regions in the texture.
    This is now done in a script file - each font must have a corresponding
    .mapping script performing the mapping.

--- Polys/src/Game/States/PlayState.cs
+++ Polys/src/Game/States/PlayState.cs
@@ -10,61 +10,9 @@ namespace Polys.Game.States
     {
         CharacterController controller = new CharacterController(30);
         Video.Camera camera = new Video.Camera();
-        Player player = new Player("Anima", new Video.DrawableSprite("assets/sprites/player.bmp",0,0, 16, 32));
+        Player player = new Player("Anima", new Video.DrawableSprite("assets/sprites/player.bmp", 0, 0, 16, 32));
 
-        Video.Font font = new Video.Font("assets/fonts/default.bmp", 8, 16,
-            new Dictionary<char, Util.Pair<int, int>>()
-            {
-                { 'a', new Util.Pair<int, int>(1, 3) },
-                { 'b', new Util.Pair<int, int>(1, 1) },
-                { 'c', new Util.Pair<int, int>(1, 1) },
-                { 'd', new Util.Pair<int, int>(1, 1) },
-                { 'e', new Util.Pair<int, int>(1, 1) },
-                { 'f', new Util.Pair<int, int>(1, 1) },
-                { 'g', new Util.Pair<int, int>(1, 1) },
-                { 'h', new Util.Pair<int, int>(1, 1) },
-                { 'i', new Util.Pair<int, int>(1, 1) },
-                { 'j', new Util.Pair<int, int>(1, 1) },
-                { 'k', new Util.Pair<int, int>(1, 1) },
-                { 'l', new Util.Pair<int, int>(1, 1) },
-                { 'm', new Util.Pair<int, int>(1, 1) },
-                { 'n', new Util.Pair<int, int>(1, 1) },
-                { 'o', new Util.Pair<int, int>(1, 1) },
-                { 'p', new Util.Pair<int, int>(1, 1) },
-                { 'q', new Util.Pair<int, int>(1, 1) },
-                { 'r', new Util.Pair<int, int>(1, 1) },
-                { 's', new Util.Pair<int, int>(1, 1) },
-                { 't', new Util.Pair<int, int>(1, 1) },
-                { 'u', new Util.Pair<int, int>(1, 1) },
-                { 'v', new Util.Pair<int, int>(1, 1) },
-                { 'A', new Util.Pair<int, int>(5, 1) },
-                { 'B', new Util.Pair<int, int>(6, 1) },
-                { 'C', new Util.Pair<int, int>(7, 1) },
-                { 'D', new Util.Pair<int, int>(8, 1) },
-                { 'E', new Util.Pair<int, int>(9, 1) },
-                { 'F', new Util.Pair<int, int>(10, 1) },
-                { 'G', new Util.Pair<int, int>(11, 1) },
-                { 'H', new Util.Pair<int, int>(12, 1) },
-                { 'I', new Util.Pair<int, int>(13, 1) },
-                { 'J', new Util.Pair<int, int>(14, 1) },
-                { 'K', new Util.Pair<int, int>(15, 1) },
-                { 'L', new Util.Pair<int, int>(16, 1) },
-                { 'M', new Util.Pair<int, int>(17, 1) },
-                { 'N', new Util.Pair<int, int>(18, 1) },
-                { 'O', new Util.Pair<int, int>(19, 1) },
-                { 'P', new Util.Pair<int, int>(20, 1) },
-                { 'Q', new Util.Pair<int, int>(21, 1) },
-                { 'R', new Util.Pair<int, int>(22, 1) },
-                { 'S', new Util.Pair<int, int>(23, 1) },
-                { 'T', new Util.Pair<int, int>(24, 1) },
-                { 'U', new Util.Pair<int, int>(25, 1) },
-                { 'V', new Util.Pair<int, int>(26, 1) },
-                { 'W', new Util.Pair<int, int>(27, 1) },
-                { 'X', new Util.Pair<int, int>(28, 1) },
-                { 'Y', new Util.Pair<int, int>(29, 1) },
-                { 'Z', new Util.Pair<int, int>(30, 1) },
-
-            });
+        Video.Font font = new Video.Font("assets/fonts/default.bmp", 8, 16);
 
         public PlayState()
         {
@@ -86,7 +34,7 @@ namespace Polys.Game.States
 
             Video.HighLevelRenderer.draw(player.sprite, camera);
 
-            font.renderText("HELLO WORLD", 5, 5);
+            font.renderText("abcdefghijklmnopqrstuvwxyz.,!?ABCDEFGHIJKLMNOPQRSTUVWXYZ|}{/\\", 5, 5);
 
             return StateManager.StateRenderResult.StopDrawing;
         }
@@ -102,7 +50,7 @@ namespace Polys.Game.States
             controller.finishGatheringInput(sceneList.current.collisionLayer);
             player.updateUv();
             camera.centreOn(player.sprite.posX, player.sprite.posY);
-            
+
             return StateManager.StateUpdateResult.Finish;
         }
 
--- Polys/src/Video/Font.cs
+++ Polys/src/Video/Font.cs
@@ -1,21 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using MoonSharp.Interpreter;
 
 namespace Polys.Video
 {
     /** Represents a bitmap font in memory. A font is nearly identical to a tileset,
       * mainly differing in that each tile is a letter. */
-    class Font : Tileset
+    class Font : Tileset, IScriptInitialisable
     {
         int characterWidth, characterHeight;
         Dictionary<char, Util.Pair<int, int>> charPosMapping = new Dictionary<char, Util.Pair<int, int>>();
 
-        public Font(string path, int characterWidth, int characterHeight, Dictionary<char, Util.Pair<int, int>> charPosMapping)
+        public Font(string path, int characterWidth, int characterHeight)
           : base(path, "font", characterWidth, characterHeight)
         {
             this.characterWidth = characterWidth;
             this.characterHeight = characterHeight;
-            this.charPosMapping = charPosMapping;
+            ScriptManager.initialiseFromScript(System.IO.Path.ChangeExtension(path, ".mapping"), this);
+        }
+
+        public void InitialiseFromScript(Table table)
+        {
+            foreach(var entry in table.Pairs)
+            {
+                //Test that the value is a valid table.
+                if (entry.Key.Type!=DataType.String ||
+                    entry.Key.String.Length != 1 ||
+                    entry.Value.Type!=DataType.Table ||
+                    entry.Value.Table.Length!=2 ||
+                    entry.Value.Table.Get(DynValue.NewNumber(1)).Type != DataType.Number ||
+                    entry.Value.Table.Get(DynValue.NewNumber(2)).Type != DataType.Number)
+                    continue;
+
+                int x = (int)entry.Value.Table.Get(DynValue.NewNumber(1)).Number;
+                int y = (int)entry.Value.Table.Get(DynValue.NewNumber(2)).Number;
+
+                charPosMapping.Add(entry.Key.String[0], new Util.Pair<int, int>(x, y));
+            }
         }
 
         public void renderText(string text, int positionX, int positionY)
@@ -32,6 +53,11 @@ namespace Polys.Video
 
                 HighLevelRenderer.draw(sprite, this, new Camera());
             }
+        }
+
+        public string ScriptName()
+        {
+            return "map";
         }
     }
 }
