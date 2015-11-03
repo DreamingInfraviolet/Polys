925d540175042c9794af62efe2ab1f92f3945b55
Anima <lightofanima@gmail.com>
2015-11-03 16:11:21 +0000

    Added to quadtree implementation

--- Polys/src/Util/Quadtree.cs
+++ Polys/src/Util/Quadtree.cs
@@ -4,45 +4,92 @@ using OpenGL;
 
 namespace Polys.Util
 {
-    /*
     public class Quadtree
     {
         Node root;
-        int maxObjects, maxLevels;
+        int maxObjects;
         int level;
 
         public Vector2 origin { get; private set; }
 
-        public Quadtree(int maxObjects = 5, int maxLevels = 5)
+        public Quadtree(Rect rect, int maxObjects = 15)
         {
-            root = new Node();
+            if ((rect.w & (rect.w - 1)) == 0 || (rect.h & (rect.h - 1)) == 0 || rect.w == 0 || rect.h == 0)
+                throw new Exception("Attempting to create a quadtree with non power-of-two size.");
+
+            root = new Node(rect, maxObjects);
             this.maxObjects = maxObjects;
-            this.maxLevels = maxLevels;
         }
-        
 
-        public Node closest(Video.Transformable t)
+        public void clear()
         {
-            if (!root.hasChildren())
-                return null;
+            root.clear();
+        }
 
-            return root.find(x, y);
+        public void insert(Video.Transformable t)
+        {
+
         }
     }
 
     public class Node
     {
-        Video.Transformable obj;
+        Video.Transformable[] objects;
 
         Node topRight, topLeft, bottomLeft, bottomRight;
+        Rect rect;
 
-        public Node(bool valid = false) {}
+        int objectCount = 0;
+        bool hasChildren = false;
 
- 
-        public bool hasChildren()
+        public Node(Rect r, int maxObjects)
         {
-            return topLeft != null || bottomLeft != null || topRight != null || bottomRight != null;
+            rect = r;
+            objects = new Video.Transformable[maxObjects];
+        }
+
+        public void insert(Video.Transformable t)
+        {
+            //If the object is bigger than half the size of the node, insert it into this node.
+            //Else, insert it into a subnode.
+        }
+
+        public void split()
+        {
+            if (!hasChildren)
+            {
+                if (rect.w == 1 || rect.h == 1)
+                    throw new Exception("Attempting to split a quadtree of size 1.");
+
+                int halfW = rect.w / 2;
+                int halfH = rect.h / 2;
+
+                topRight = new Node(new Rect(rect.x+ halfW, rect.y, halfW, halfH), objects.Length);
+                topLeft = new Node(new Rect(rect.x, rect.y, halfW, halfH), objects.Length);
+                bottomLeft = new Node(new Rect(rect.x, rect.y + halfH, halfW, halfH), objects.Length);
+                bottomRight = new Node(new Rect(rect.x+ halfW, rect.h+halfH, halfW, halfH), objects.Length);
+                hasChildren = true;
+            }
+        }
+
+        public void clear()
+        {
+            if (hasChildren)
+            {
+                topRight.clear();
+                topLeft.clear();
+                bottomLeft.clear();
+                bottomRight.clear();
+
+                topRight = topLeft = bottomLeft = bottomRight = null;
+
+                hasChildren = false;
+            }
+
+            for (int i = 0; i < objects.Length; ++i)
+                objects[i] = null;
+
+            objectCount = 0;
         }
     }
-    */
 }
